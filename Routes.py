c15c8b8166081f4bd934a43fe3c361f96c8f6599
ayush <ayushgd@gmail.com>
2020-06-27 14:22:32 +0530

    created wtf form for the login page

--- Routes.py
+++ Routes.py
@@ -1,6 +1,16 @@
 from hms import app
-from flask import render_template, session, url_for, request, redirect
+from flask import render_template, session, url_for, request, redirect, flash
+from .Forms import Login_form
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def main():
-    return render_template('login.html', title="Login")
+    form=Login_form()
+    if form.validate_on_submit():
+        flash("login successful")
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title="Login", form=form)
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
