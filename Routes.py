4e356c477af4037e2f66730c84e028427e5cf305
ayush <ayushgd@gmail.com>
2020-06-28 00:18:17 +0530

    alert and flash mechanism changed and form for patient creation updated with wtf

--- Routes.py
+++ Routes.py
@@ -1,7 +1,7 @@
 from hms import app
 from flask import render_template, session, url_for, request, redirect, flash, session
-from .Forms import Login_form
-from Models import UserStore, Patient_test, Patient_Medicine, Patient_details, Diagnosis, Medicine
+from .Forms import Login_form,Patient_create
+#from Models import UserStore, Patient_test, Patient_Medicine, Patient_details, Diagnosis, Medicine
 
 @app.route("/",methods=["GET","POST"])
 @app.route("/login",methods=["GET","POST"])
@@ -15,9 +15,9 @@ def main():
                 flash("login successful")
                 #g.user = "Admin"
                 session['username'] = request.form.get('username')
-                return render_template('create_patient.html', alert='success', title="Login", form=form)
+                return redirect(url_for('create_patient'))
             else:
-                flash("login failed")
+            
                 return render_template('login.html', alert='failed', title="Login", form=form)
     return render_template('login.html', title="Login", form=form)
 
@@ -29,7 +29,8 @@ def index():
 def create_patient():
     if not session["username"]:
         return redirect('login')
-    return render_template("create_patient.html", title="Create Patient")
+    form=Patient_create()
+    return render_template("create_patient.html", title="Create Patient",form=form)
 
 @app.route("/DeletePatient")
 def delete_patient():
