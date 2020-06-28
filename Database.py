cc125e3e533c05fea534c9d9800f90c86f79641b
RamVignesh B <ramvigneshgreat@pec.edu>
2020-06-28 07:50:51 +0530

    Formatting

--- Database.py
+++
@@ -1,7 +0,0 @@
-from Models import UserStore, Patient_test, Patient_Medicine, Patient_details, Diagnosis, Medicine
-from Config import db
-
-def create_p(details):
-    db.session.add(Patient_details(details))
-    db.session.commit()
-    return True
--- Routes.py
+++ Routes.py
@@ -5,38 +5,39 @@ from .Forms import Login_form
 from .Models import UserStore, Patient_test, Patient_Medicine, Patient_details, Diagnosis, Medicine
 from .Config import db
 
-#from Database import create_p
 
 
-@app.route("/",methods=["GET","POST"])
-@app.route("/login",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def main():
     if 'username' in session:
         return render_template('index.html', user=session['username'])
-    form=Login_form()
+    form = Login_form()
     if request.method == 'POST':
-        #Validate the form
+        # Validate the form
         if form.validate_on_submit():
-            #Check the credentials
+            # Check the credentials
             if request.form.get('username') == 'admin' and request.form.get('password') == 'admin':
                 flash("login successful")
                 #g.user = "Admin"
                 session['username'] = request.form.get('username')
                 return redirect(url_for('create_patient'))
             else:
-            
+
                 return render_template('login.html', alert='failed', title="Login", form=form)
     return render_template('login.html', title="Login", form=form)
+
 
 @app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route("/CreatePatient", methods=['GET','POST'])
+
+@app.route("/CreatePatient", methods=['GET', 'POST'])
 def create_patient():
     if 'username' not in session or not session['username']:
         return redirect('login')
-    #If form has been submitted
+    # If form has been submitted
     if request.method == 'POST':
         ssn_id = request.form.get('ssn_id')
         name = request.form.get('patient_name')
@@ -47,17 +48,20 @@ def create_patient():
         state = request.form.get('state_list')
         city = request.form.get('stt')
         #create_p([ssn_id, name, age, date, bed_type, address, state, city])
-        details = Patient_details(name, age, ssn_id, date, bed_type, address, city, state, status="Admitted")
+        details = Patient_details(
+            name, age, ssn_id, date, bed_type, address, city, state, status="Admitted")
         db.session.add(details)
         db.session.commit()
         flash("Succefully Created")
     return render_template("create_patient.html", title="Create Patient")
+
 
 @app.route("/DeletePatient")
 def delete_patient():
     if 'username' not in session:
         return redirect('login')
     return render_template("delete_patient.html", title="Delete Patient")
+
 
 @app.route("/UpdatePatient")
 def update_patient():
