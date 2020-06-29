5ea59810fe2a1ebe1b8662f11de257d60165fb28
ayush <ayushgd@gmail.com>
2020-06-29 18:05:50 +0530

    update functionality

--- Routes.py
+++ Routes.py
@@ -1,7 +1,7 @@
 from hms import app
 from datetime import datetime
 from flask import render_template, session, url_for, request, redirect, flash, session,g
-from .Forms import Login_form,Patient_create,Patient_delete,delete_result
+from .Forms import Login_form,Patient_create,Patient_delete,delete_result,Patient_update
 from .Models import UserStore, Patient_test, Patient_Medicine, Patient_details, Diagnosis, Medicine
 from .Config import db
 
@@ -51,11 +51,11 @@ def create_patient():
             date = form.date.data
             bed_type = form.Type_of_bed.data
             address = form.address.data
-            state = request.form.get('state_list')
-            city = request.form.get('stt')
+            city = request.form.get('state_list')
+            state = request.form.get('stt')
             #if(Patient_details.query.filter_by())
             details = Patient_details(
-                name, age, ssn_id, date, bed_type, address, city, state, status="Admitted")
+                name, age, ssn_id, date, bed_type, address, city, state, status="Active")
             db.session.add(details)
             db.session.commit()
             flash("Patient creation initiated successfully","success")
@@ -81,6 +81,8 @@ def delete_patient():
 
 @app.route("/deletepatient2",methods=["GET","POST"])
 def delete_patient2():
+    if 'username' not in session:
+        return redirect('login')
     form2=delete_result()
     if form2.validate_on_submit():
         global pid
@@ -113,12 +115,87 @@ def search_patient():
             flash("patient not found","danger")
     return render_template("search_patient2.html", title="Search Patient",form=form)
 
-@app.route("/UpdatePatient")
+@app.route("/UpdatePatient",methods=["GET","POST"])
 def update_patient():
+    flag=0
     if 'username' not in session:
         return redirect('login')
-    return render_template("update_patient.html", title="Update Patient")
+    form=Patient_delete()
+    if form.validate_on_submit():
+        global pid 
+        pid = int(form.patient_id.data)
+        patient=Patient_details.query.filter(Patient_details.id==int(form.patient_id.data))
+        for patient_1 in patient:
+            if patient_1:
+                flash("patient found","success")
+                flag=1
+                form2=Patient_update(Type_of_bed=patient_1.bed_type,date=patient_1.admission_date,address=patient_1.address,patient_name=patient_1.name,patient_age=patient_1.age)
+                return render_template("update_patient.html",title="Update Patient",form=form,form2=form2,flag=flag,patient_s=patient)
+        flash("Patient not found","danger")
+    return render_template("update_patient.html", title="Update Patient",form=form,flag=flag)
 
+@app.route("/UpdatePatient2",methods=["GET","POST"])
+def update_result():
+    if 'username' not in session:
+        return redirect('login')
+    form=Patient_update()
+    if request.method=="POST":
+        if form.validate_on_submit():
+            global pid
+            if request.form.get('stt')!="": 
+                if request.form.get('state_list')==None or request.form.get('state_list')=="":
+                    patient=Patient_details.query.filter(Patient_details.id==pid)
+                    for patient_1 in patient:
+                        if patient_1:
+                            
+                            flag=1
+                            flash("You have to select city if you change state","danger")
+                            form2=Patient_update(Type_of_bed=patient_1.bed_type,date=patient_1.admission_date,address=patient_1.address,patient_name=patient_1.name,patient_age=patient_1.age)
+                            return render_template("update_patient.html",title="Update Patient",form=form,form2=form2,flag=flag,patient_s=patient)
+                    
+            
+            print(pid)
+            if request.form.get('stt')=="":
+                name = form.patient_name.data
+                age = form.patient_age.data
+                date = form.date.data
+                bed_type = form.Type_of_bed.data
+                address = form.address.data
+
+                Patient_details.query.filter_by(id=pid).update({"name": name})
+                Patient_details.query.filter_by(id=pid).update({"admission_date": date})
+                
+                Patient_details.query.filter_by(id=pid).update({"age": age})
+                Patient_details.query.filter_by(id=pid).update({"bed_type": bed_type})
+                Patient_details.query.filter_by(id=pid).update({"address": address})
+            else:
+                name = form.patient_name.data
+                age = form.patient_age.data
+                date = form.date.data
+                bed_type = form.Type_of_bed.data
+                address = form.address.data
+                city = request.form.get('state_list')
+                state = request.form.get('stt')
+                Patient_details.query.filter_by(id=pid).update({"name": name})
+                Patient_details.query.filter_by(id=pid).update({"admission_date": date})
+                Patient_details.query.filter_by(id=pid).update({"city": city})
+                Patient_details.query.filter_by(id=pid).update({"state": state})
+                Patient_details.query.filter_by(id=pid).update({"age": age})
+                Patient_details.query.filter_by(id=pid).update({"bed_type": bed_type})
+                Patient_details.query.filter_by(id=pid).update({"address": address})
+
+            
+            db.session.commit()
+            flash("Patient update intiated successfully ","success")
+            return redirect(url_for('update_patient'))
+        patient=Patient_details.query.filter(Patient_details.id==pid)
+        for patient_1 in patient:
+            if patient_1:
+                
+                flag=1
+                flash("Please enter age in integer format and less than or equal to 3 digits in length","danger")
+                form2=Patient_update(Type_of_bed=patient_1.bed_type,date=patient_1.admission_date,address=patient_1.address,patient_name=patient_1.name,patient_age=patient_1.age)
+                return render_template("update_patient.html",title="Update Patient",form=form,form2=form2,flag=flag,patient_s=patient)
 
 @app.route("/logout")
 def logout():
