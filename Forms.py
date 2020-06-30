69051ccea6ea833989537fdc223968a55d15ef5e
ayush <ayushgd@gmail.com>
2020-06-30 13:38:29 +0530

    issue medicine form

--- Forms.py
+++ Forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, SelectField
-from wtforms.validators import DataRequired, Email, Length, ValidationError, NumberRange
+from wtforms.validators import DataRequired, Email, Length, ValidationError
 import datetime
 from wtforms.fields.html5 import DateField
 from .Models import UserStore, Patient_test, Patient_Medicine, Patient_details, Diagnosis, Medicine
@@ -31,7 +31,7 @@ class pass_val(FlaskForm):
         if nflag==0 or cflag==0 or upflag==0:
             raise ValidationError(self.message)
         if len(x)!=10:
-            raise ValidationError("Password length must be 10 characters long")
+            raise ValidationError("Password length must be 10 charachters long")
 
 
     
@@ -58,22 +58,16 @@ class check_length(FlaskForm):
             raise ValidationError(self.message)
 
 
-    
-
 # class for patient registration form
 class Patient_create(FlaskForm):
     ssn_id = IntegerField('ssn id', validators=[DataRequired('please enter SSN ID in integer format'), check_length(message="id must be 9 digits long",min=9, max=9)])
     patient_name = StringField('patient name', validators=[ DataRequired('please enter name')])
-    patient_age = IntegerField('patient age', validators=[ DataRequired('please enter age'), check_length(min=1, max=3, message="age should be 1-3 digits long"), NumberRange(min=0, max=150, message="Age should be between 0 and 150!")])
+    patient_age = IntegerField('patient age', validators=[ DataRequired('please enter age'), check_length(min=1, max=3, message="age should be 1-3 digits long")])
     date = DateField('enter date', format="%Y-%m-%d", validators=[ DataRequired('please enter date')], default=datetime.date.today())
     Type_of_bed = SelectField('bed type', choices=[('General ward', 'General ward'), ('Semi sharing', 'Semi sharing'), ('single room', 'single room')], validators=[DataRequired('select ward type')])
     address = StringField('enter address', validators=[DataRequired('enter the address')])
     submit= SubmitField('create')
 
-    #Validate date
-    def validate_date(form, date):
-        if date.data > datetime.date.today():
-            raise ValidationError("Date of Admission cannot exceed today's date!")
     
 #class for delete patient form
 class Patient_delete(FlaskForm):
@@ -92,3 +86,38 @@ class Patient_update(FlaskForm):
     submit= SubmitField('update')
 
 
+#class for medicine issuing
+class issue_medicine(FlaskForm):
+    medicine_name=StringField('Medicine name',validators=[DataRequired('Please enter medicine name '), Alpha('medicine name has to be alphabet only')])
+    quantity=IntegerField('QUANTITY',validators=[DataRequired('Please enter quantity'),check_med()])
+
+class   Apha(FlaskForm):
+    def __init__(self,message):
+        if not message:
+            message="input should contain alphabets only"
+        self.message=message
+    
+    def __call__(self,form,field):
+        name=str(field.data)
+        if not name.isalpha():
+            raise ValidationError('self.message')
+        
+class check_med(Flaskform):
+    def init(self,message):
+        if not message:
+            message="medicine unavailable"
+        medicine=self.message
+    def __call__(self,form,field):
+        name=form.medicine_name.data
+        quant=field.data
+        if Medicine.query.filter(Medicine.medicine_name==name).first()==None:
+            raise ValidationError('MEDICINE NOT FOUND IN DATABASE')
+        medicines=Medicine.query.filter(Medicine.medicine_name==name).first()
+        for medicine in medicines:
+            if quant>medicine.medicine_quantity:
+                raise ValidationError("Medicine quantity is more than available. Available quantiy={}".format(medicine.medicine_quantity))
+        
+            
+         
+
+    
